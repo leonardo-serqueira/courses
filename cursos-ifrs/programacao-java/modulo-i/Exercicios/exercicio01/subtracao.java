5a70ac3ff7f861515788850d8e22913a12545f5b
Leonardo Serqueira <leonardoserqueira@hotmail.com>
2026-05-26 16:44:15 -0300

    exercicio 1 da lista

---
+++ cursos-ifrs/programacao-java/modulo-i/Exercicios/exercicio01/subtracao.java
@@ -0,0 +1,21 @@
+public class subtracao{
+
+    public static void main(String[] args){
+        
+        double num1, num2;
+
+        System.out.println("\n------------------- PROGRAMA DE SUBTRACAO -------------------\n");
+        
+        System.out.print("Informe o primeiro numero: ");
+        num1 = Double.parseDouble(System.console().readLine());
+
+        System.out.print("Informe o segundo numero: ");
+        num2 = Double.parseDouble(System.console().readLine());
+        
+
+        double subtr = num1 - num2;
+        System.out.printf("\n------------------- RESULTADO: -------------------\n \n %.2f - %.2f = %.2f\n", num1, num2, subtr);
+        
+
+    }
+}
