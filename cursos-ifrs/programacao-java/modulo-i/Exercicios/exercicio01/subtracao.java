00471ab619e89817c684b0fb116488b5da3963c3
Leonardo Serqueira <leonardoserqueira@hotmail.com>
2026-05-26 20:06:17 -0300

    adicao de enunciados

--- cursos-ifrs/programacao-java/modulo-i/Exercicios/exercicio01/subtracao.java
+++ cursos-ifrs/programacao-java/modulo-i/Exercicios/exercicio01/subtracao.java
@@ -1,3 +1,8 @@
+/*
+    1. Faça um programa que receba dois números, calcule e mostre a
+    subtração do primeiro número pelo segundo.
+*/
+
 public class subtracao{
 
     public static void main(String[] args){
