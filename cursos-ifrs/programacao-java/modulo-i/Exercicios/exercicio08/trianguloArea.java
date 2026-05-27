1136a64bebd11972eb128d549555296a74e07e9c
Leonardo Serqueira <leonardoserqueira@hotmail.com>
2026-05-27 11:05:43 -0300

    exercicio 08

---
+++ cursos-ifrs/programacao-java/modulo-i/Exercicios/exercicio08/trianguloArea.java
@@ -0,0 +1,27 @@
+/*
+    8. Faça um programa que calcule e mostre a área de um triângulo.
+     Sabese que: Área = (base x altura) / 2.
+     A base e altura devem ser informadas pelo usuário.
+*/
+public class trianguloArea {
+
+    public static void main(String[] args){
+
+        double base, altura, area;
+
+        System.out.println("\nPROGRAMA AREA DE TRIANGULO");
+
+        System.out.print("\nDigite a base do triangulo: ");
+        base = Double.parseDouble(System.console().readLine());
+
+        System.out.print("\nDigite a altura do triangulo: ");
+        altura = Double.parseDouble(System.console().readLine());
+        
+        area = (base * altura) /2;
+
+        System.out.println("\nArea do seu triangulo: "+
+        String.format("%.2f", area));
+
+    }
+    
+}
