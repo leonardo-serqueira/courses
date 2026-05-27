bec03e634a98e3d5629cc757a952014b39bd8c0d
Leonardo Serqueira <leonardoserqueira@hotmail.com>
2026-05-27 09:48:59 -0300

    exercicio 06

---
+++ cursos-ifrs/programacao-java/modulo-i/Exercicios/exercicio06/comissao.java
@@ -0,0 +1,34 @@
+/*
+    6. Um funcionário recebe um salário fixo mais 4% de comissão sobre as
+    vendas (4% do valor das vendas efetuadas pelo funcionário). Faça um
+    programa que receba o salário fixo do funcionário e o valor de suas
+    vendas, calcule e mostre a comissão e seu salário final.
+*/
+
+public class comissao {
+    public static void main(String[] args){
+
+        double sal, venda, comis, total;
+
+        System.out.println("\nPROGRAMA CALCULADOR DE COMISSAO");
+
+        System.out.print("\nInsira seu salario:");
+        sal = Double.parseDouble(System.console().readLine());
+
+        System.out.print("\nInsira o valor da venda: ");
+        venda = Double.parseDouble(System.console().readLine());
+
+        comis = (venda * 4) / 100;
+        total = sal + comis;
+
+        System.out.println("\nRESULTADO:");
+
+        System.out.printf(
+        "\nSeu salario       : %.2f"+
+        "\nSua venda         : %.2f"+
+        "\nSua comissao(4%%)  : %.2f"+
+        "\nSalario + comissao: %.2f\n",
+        sal, venda, comis, total);
+
+    }
+}
